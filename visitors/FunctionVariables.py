435bda5688d835f9ea409d3c41bffcca3ed2aacb
Jennifer Ye <yej52@mcmaster.ca>
2022-12-06 14:24:04 -0500

    adds function local vars, args and return visitor and generator

--- visitors/FunctionVariables.py
+++ visitors/FunctionVariables.py
@@ -10,9 +10,11 @@ class LocalVariableExtraction(GlobalVariableExtraction):
 
     def __init__(self, vars) -> None:
         super().__init__()
-        self.local = set()  
-        self.param = set() # tuple()
+        self.local = dict()  
+        self.args = set() 
         self.vars = vars
+        self.r_num = 0
+        self.re = False
         self.gen = self.get_name()
         
     def visit_Assign(self, node):
@@ -20,34 +22,14 @@ class LocalVariableExtraction(GlobalVariableExtraction):
             raise ValueError("Only unary assignments are supported")
 
         var = node.targets[0].id
-        if (var not in self.vars):
-            if (len(var)> 8):  #checks if the variable name is greater than 8 characters long
-                name = self.get_next()
-                if (var[0] == '_'): #preserves constant naming convention
-                    if len(name) > 8:
-                        var = var[0]+(name[:-1]).upper()
-                    else:
-                        var = var[0]+name.upper()
-                else:
-                    var = name
-            self.vars[node.targets[0].id] = var
-                    
-            node.targets[0].id = var
-
-            if hasattr(node.value, 'value'):
-                self.local.add((node.targets[0].id, node.value.value))
-            else:
-                self.local.add(node.targets[0].id)
-
-    def visit_While(self, node):
-        if(node.test.left.id in self.vars): # ensure iterator variables are set to 1 by default
-            for var in self.local: # must iterate since set of tuples and strings
-                if (type(var) is tuple) and (var[0] == node.test.left.id and var[1] < 1): 
-                    self.local.remove(var)
-                    self.local.add((node.test.left.id, 1))
-
-        for contents in node.body:
-            self.visit(contents)
+        if (var in self.vars or len(var) > 8): # rename
+            name = self.get_next()
+            var = name                
+            # self.vars[node.targets[0].id] = var
+            self.local[node.targets[0].id] = var
+        else:
+            self.local[node.targets[0].id] = var
+        
 
     def visit_FunctionDef(self, node):
         arguments = node.args.args
@@ -59,15 +41,23 @@ class LocalVariableExtraction(GlobalVariableExtraction):
             self.visit(contents)
     
     def visit_arg(self, node):
+        var = node.arg
         if (var not in self.vars):
             if (len(var)> 8):  #checks if the variable name is greater than 8 characters long
                 name = self.get_next()
                 var = name
-            self.vars[node.targets[0].id] = var
-                    
-            node.targets[0].id = var
-        self.param.add(node.arg) 
+            self.vars[node.arg] = var
+            node.arg = var
+        
+        self.args.add(node.arg) 
     
+    def visit_Return(self, node):
+        self.re = 'r'+str(self.retun_num())
+        # self.vars[self.re] = ''
+
+    ####
+    ## Helper functions for renaming generator
+    ####
     def get_name(self):
         i = 0  # length of new rename
         possible = []
@@ -84,3 +74,7 @@ class LocalVariableExtraction(GlobalVariableExtraction):
             next_name = next(self.gen)
 
         return next_name
+    
+    def retun_num(self):
+        self.r_num = self.r_num + 1
+        return self.r_num
